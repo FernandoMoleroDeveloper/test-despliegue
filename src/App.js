a40fcb4ce367253477d421a2c766f720e785b6a3
FernandoMoleroDeveloper <118685226+FernandoMoleroDeveloper@users.noreply.github.com>
2023-03-23 20:31:27 +0100

    prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React, { useEffect } from 'react';
+import { HashRouter, Link, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import NotesPage from './pages/NotesPage';
+import TogglablePage from './pages/TogglablePage';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [images, setImages] = React.useState([]);
+
+  // Ask the api images
+  useEffect(() => {
+    const options = { headers: { Authorization: API_KEY } };
+    const finalUrl = API_URL + '?query=car';
+    fetch(finalUrl, options)
+      .then((data) => data.json())
+      .then((dataParsed) => {
+        setImages(dataParsed.photos);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hola amigos!</h1>
+
+      <HashRouter>
+        <div className='links'>
+          <Link to='/'>HomePage</Link>
+          <br />
+          <Link to='/notes'>NotesPage</Link>
+          <br />
+          <Link to='/toggles'>TogglablePage</Link>
+          <br />
+        </div>
+
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>}></Route>
+          <Route path='/notes' element={<NotesPage></NotesPage>}></Route>
+          <Route path='/toggles' element={<TogglablePage></TogglablePage>}></Route>
+        </Routes>
+      </HashRouter>
+
+      <p>Imágenes:</p>
+      {images.map((image) => (
+        <img key={image.id} src={image.src.small} />
+      ))}
     </div>
   );
 }
